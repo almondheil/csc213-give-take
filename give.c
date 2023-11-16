e18bad5fb383e1e3afca654616ff272b3397e68c
almond heil <almond@almendra.dev>
2023-11-16 10:34:44 -0600

    Add basic argument checking to give and take

--- give.c
+++ give.c
@@ -1,12 +1,32 @@
+#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
 int main(int argc, char ** argv) {
+	// Check the right number of arguments have been supplied
 	if (argc != 3) {
-		printf("Usage: %s USER FILE\n", argv[0]);
+		fprintf(stderr, "Usage: %s USER FILE\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
-	printf("This is the give program, it sure is running\n");
+	// If the user trying to send to themselves, don't let them
+	char * give_username = getenv("LOGNAME");
+	if (strcmp(give_username, argv[1]) == 0) {
+		fprintf(stderr, "Error: Cannot give a file to yourself!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Check that the user they are trying to send to exists
+	struct passwd * to_user = getpwnam(argv[1]);
+	if (to_user == NULL) {
+		fprintf(stderr, "Error: Could not find the user %s!\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	// TODO: Later on, check that argv[2] is a valid file when we try to open it
+
+	printf("Right now, the program would give the file %s to the user %s\n", argv[2], argv[1]);
 	return 0;
 }
--- take.c
+++ take.c
@@ -1,12 +1,30 @@
+#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
 int main(int argc, char ** argv) {
-	if (argc != 3) {
+	// TODO: Later, I may want a case for just running with argc == 1 -- list out pending files
+	if (argc != 2) {
 		printf("Usage: %s USER\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
-	printf("This is the take program, it sure is running\n");
+	// If the user trying to take from themselves, don't let them
+	char * take_username = getenv("LOGNAME");
+	if (strcmp(take_username, argv[1]) == 0) {
+		fprintf(stderr, "Error: Cannot take a file from yourself!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Check that the user they are trying to take from exists
+	struct passwd * from_user = getpwnam(argv[1]);
+	if (from_user == NULL) {
+		fprintf(stderr, "Error: Could not find the user %s!\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Right now, the program would take files from the user %s\n", argv[1]);
 	return 0;
 }
