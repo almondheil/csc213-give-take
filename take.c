b9d4b260de93c18d14eafdc8d1338b28f7d695b4
Almond Heil <heilalmond@grinnell.edu>
2023-11-26 13:29:18 -0600

    Remove unused #include's

--- take.c
+++ take.c
@@ -1,13 +1,6 @@
-#include <errno.h>
-#include <fcntl.h>
-#include <pthread.h>
-#include <pwd.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #include "message.h"
