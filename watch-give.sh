d5e7d422b83691d6968480e369ffe0801b927fdc
Almond Heil <heilalmond@grinnell.edu>
2023-11-26 23:33:26 -0600

    Add comments to watch-give.sh

--- watch-give.sh
+++ watch-give.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+# A script that runs watch to show you any processes running give, excluding
+# those involved in the watch command (grep, watch, and sh).
+# Useful for noticing if any processes refuse to die.
+
 watch -n 1 'ps -aux | grep give | grep -v -E "watch|sh|grep"'
